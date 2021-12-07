3adad11b6d719c2547c26900825a6705c3f64b82
Anand Upadhya <anand.upadhya@yahoo.com>
2021-12-07 15:10:27 +0800

    Add icons and change markers

--- pages/toilets/index.js
+++ pages/toilets/index.js
@@ -107,7 +107,7 @@ Page({
             borderRadius: 4,
             padding: 8,
           }
-          return { ...toilet, width: '40', height: '40', iconPath: '/images/marker.png', callout, happy, unhappy }
+          return { ...toilet, width: '42', height: '42', iconPath: '/images/marker2.png', callout, happy, unhappy }
         };
         
         const toilets = res.data.toilets.map((toilet) => {
